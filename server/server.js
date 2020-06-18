d6737273195e088e414b8dc6ebdf64a4af28e830
Rizwan Choudhury <choudhuryrizwan97@gmail.com>
2020-06-18 14:11:58 -0700

    add server functionality

--- server/server.js
+++ server/server.js
@@ -1,9 +1,26 @@
-const faker = require('faker');
+const express = require('express');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const cors = require('cors');
+const path = require('path');
+const port = 3500;
 
-let names = [];
-for (var i = 0; i < 3; i++) {
-  var randomName = faker.name.firstName();
-  names.push(randomName);
-}
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(morgan('dev'));
 
-console.log(names);
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+// const faker = require('faker');
+
+// let names = [];
+// for (var i = 0; i < 3; i++) {
+//   var randomName = faker.name.firstName();
+//   names.push(randomName);
+// }
+
+// console.log(names);
