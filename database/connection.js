9ddeb38fcf42e40b79ea5eae86f35980a3355d26
Rizwan Choudhury <choudhuryrizwan97@gmail.com>
2020-06-18 14:45:48 -0700

    add seed function

--- database/connection.js
+++ database/connection.js
@@ -1,8 +1,19 @@
-import mongo from 'mongo';
+const Mongo, { MongoClient } = require('mongodb');
+
+let url = 'mongodb://localhost:27017/Nav_Bar';
 
 use Nav_Bar;
+
+MongoClient.connect(url, function (err, db) {
+  if (err) throw err;
+  console.log("Database created!");
+  db.close();
+});
+
+let db = connection.getDB("Nav_Bar");
 
 Nav_Bar.createCollection("Menu");
 
 Nav_Bar.createCollection("Search");
 
+module.exports = db;
