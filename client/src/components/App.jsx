4ab37929a6566924841dad98a488ec20aba3ddcc
Rizwan Choudhury <choudhuryrizwan97@gmail.com>
2020-06-23 13:18:01 -0700

    add app-subcomponents files and linked to App.jsx

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import React from 'react';
-import React from 'react';
-import React from 'react';
+import Universal-Banner from './App-Subcomponents/Universal-Banner.jsx';
+import Header from './App-Subcomponents/Header.jsx';
+import Main-Nav from './App-Subcomponents/Main-Nav.jsx';
 
 class App extends React.Component {
   constructor(props) {
