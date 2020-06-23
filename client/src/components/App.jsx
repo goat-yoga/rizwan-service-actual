57d61c04ac54b5539ad8f63bdb5136eaa00218d7
Rizwan Choudhury <choudhuryrizwan97@gmail.com>
2020-06-23 13:14:58 -0700

    add structure to App.jsx:

---
+++ client/src/components/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import React from 'react';
+import React from 'react';
+import React from 'react';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {}
+  }
+
+  render() {
+    return (
+      <div>
+        <Universal-Banner />
+        <Header />
+        <Main-Nav />
+      </div>
+    )
+  }
+}
