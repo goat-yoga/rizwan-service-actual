36858df1b066f3e02262458ac91a58468d8583d4
Rizwan Choudhury <choudhuryrizwan97@gmail.com>
2020-06-23 13:43:41 -0700

    create messages

--- client/src/components/App-Subcomponents/Universal_Banner.jsx
+++ client/src/components/App-Subcomponents/Universal_Banner.jsx
@@ -3,8 +3,17 @@ import React from 'react';
 export default class Universal_Banner extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {}
+    this.state = {
+      messages: [
+        'NEW! FRESH BLUE 💙',
+        'Spend $250 and get the Iconic Shopper Tote ($48 value)',
+        'GUYS, YOU NEED THIS NEWNESS RN',
+        'NEW KICKS JUST DROPPED!'
+      ],
+      messageIndex: 0
+    }
   }
+
 
   render() {
     return (
