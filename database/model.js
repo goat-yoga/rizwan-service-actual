9b9c52a30ba7afe2bb125fd5931fb9ad0b1ed6c1
Rizwan Choudhury <choudhuryrizwan97@gmail.com>
2020-06-24 15:42:39 -0700

    using promises for get request in search

--- database/model.js
+++ database/model.js
@@ -1,12 +1,76 @@
 const { productModel, popularSuggestionsModel, categoryModel, pagesModel } = require('./index.js');
 
 let model = {
-  fetchSearchData: (callback) => {
-    productModel.find({})
-      .exec((err, result) => {
-        err ? callback(err, null) : callback(null, result)
+  // fetches search result
+  fetchSearchData: (body, callback) => {
+    let { search } = body
+    // stores search data here
+    let searchData = {}
+
+    // adds products from DB to searchData
+    productModel.find({
+      name: {
+        $regex: `${search}`,
+        $options: 'i'
+      }
+    }).limit(3).exec()
+      .then((results) => {
+        searchData.products = results;
       })
+      .then(() => {
+        pagesModel.find({
+          page_name: {
+            $regex: `${search}`,
+            $options: 'i'
+          }
+        }).limit(3).exec()
+          .then((results) => {
+            searchData.pages = results;
+          })
+          .then()
+
+      })
+      .catch(err => console.log(err))
+
+    // console.log(searchData)
+    // // adds pages from DB to searchData
+    // pagesModel.find({
+    //   page_name: {
+    //     $regex: `${search}`,
+    //     $options: 'i'
+    //   }
+    // }).limit(3)
+    //   .exec((err, result) => {
+    //     err ? callback(err, null) : searchData.pages = result
+    //   })
+
+    // // adds category from DB to searchData
+    // categoryModel.find({
+    //   page_name: {
+    //     $regex: `${search}`,
+    //     $options: 'i'
+    //   }
+    // }).limit(3)
+    //   .exec((err, result) => {
+    //     err ? callback(err, null) : searchData.categories = result
+    //   })
+
+    // // adds pages from DB to searchData
+    // popularSuggestionsModel.find({
+    //   page_name: {
+    //     $regex: `${search}`,
+    //     $options: 'i'
+    //   }
+    // }).limit(3)
+    //   .exec((err, result) => {
+    //     err ? callback(err, null) : searchData.popularSuggestions = result
+    //   })
   }
 };
 
+model.fetchSearchData({ search: 'ba' }, (err, searchResults) => {
+  err ? console.log(err) : res.status(200).json(searchResults);
+})
+
 module.exports = model;
+
