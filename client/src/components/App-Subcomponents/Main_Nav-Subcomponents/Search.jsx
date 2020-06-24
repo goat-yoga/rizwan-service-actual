01224e34f82de3d374a85f7ddfc3084f5d4c8537
Rizwan Choudhury <choudhuryrizwan97@gmail.com>
2020-06-24 16:25:45 -0700

    Changes state to mimic search functionality of site

--- client/src/components/App-Subcomponents/Main_Nav-Subcomponents/Search.jsx
+++ client/src/components/App-Subcomponents/Main_Nav-Subcomponents/Search.jsx
@@ -5,28 +5,40 @@ export default class Search extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      searchResults: ''
+      searchResults: '',
+      popularSuggestions: [],
+      categories: [],
+      pages: [],
+      products: []
     }
     this.handleSearch = this.handleSearch.bind(this);
     this.handleKeyPress = this.handleKeyPress.bind(this);
-    // this.fetchSearchData = this.fetchSearchData.bind(this);
+    this.fetchSearchData = this.fetchSearchData.bind(this);
   }
 
-  // TODO: send get request to display items when searching
-
   // Execute this function when user presses enter on search
-  // fetchSearchData() {
-  //   axios
-  //     .get(`/search`, { search: this.state.searchResults })
-  //     .then(response => setstate to many states and display it as search bar)
-  // }
+  fetchSearchData() {
+    axios
+      .get(`/search`, {
+        params: { search: this.state.searchResults }
+      })
+      .then(response => {
+        let { categories, pages, popularSuggestions, products } = response.data
+        // ES6 Magic!! same as setting {pages: pages}
+        this.setState({ popularSuggestions, categories, pages, products })
+      })
+  }
 
+  // TODO: get rid of handleKeyPress later
+  // and implement fetchSearchData in this function
+  // handles changes to search input
   handleSearch(e) {
     this.setState({ searchResults: e.target.value })
   }
 
+  // pressing enter displays data
   handleKeyPress(e) {
-    if (event.key === 'Enter') { console.log('Pressed Enter!') }
+    if (event.key === 'Enter') { this.fetchSearchData() }
   }
 
   // TODO: Have to render dropdown menu with DB items when searching
