e25795f3b3415b35e4f20e6e954cb533b7862970
Rizwan Choudhury <choudhuryrizwan97@gmail.com>
2020-06-26 10:15:44 -0700

    create hovering menu effect, and accessory menu

--- client/src/components/App-Subcomponents/Main_Nav-Subcomponents/Accessories.jsx
+++ client/src/components/App-Subcomponents/Main_Nav-Subcomponents/Accessories.jsx
@@ -1,11 +1,39 @@
 import React from 'react';
+import Acc_Menu from './Accessories_Menu.jsx'
 
-const Accessories = (props) => {
-  return (
-    <li className="main-nav__item">
-      {props.name}
-    </li>
-  )
+export default class Accessories extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      onHover: false
+    }
+    this.displayMenu = this.displayMenu.bind(this);
+    this.handleHover = this.handleHover.bind(this);
+  }
+
+  displayMenu() {
+    if (this.state.onHover) {
+      <Acc_Menu />
+    }
+  }
+
+  componentDidUpdate(props) {
+    this.displayMenu();
+  }
+
+  handleHover() {
+    this.setState({ onHover: !this.state.onHover })
+  }
+
+  render() {
+    return (
+      <li className="main-nav__item"
+        onMouseOver={this.handleHover}
+        onMouseOut={this.handleHover}>
+        <a href="">{this.props.name}</a>
+
+        <div className="main-nav__column"> </div>
+      </li >
+    )
+  }
 }
-
-export default Accessories;
---
+++ client/src/components/App-Subcomponents/Main_Nav-Subcomponents/Accessories_Menu.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Accessories_Menu = (props) => {
+  return (
+    <a href="">Accessories</a>
+    (<ul>
+      <li className="main-nav__item">
+        <a href="">Gift Card</a></li>
+      <li className="main-nav__item">
+        <a href="">Books</a></li>
+      <li className="main-nav__item">
+        <a href="">Shoes & Socks</a></li>
+      <li className="main-nav__item">
+        <a href="">Hats & Hair Accessories</a></li>
+      <li className="main-nav__item">
+        <a href="">Yoga Mats & Essentials</a></li>
+      <li className="main-nav__item">
+        <a href="">Bags</a></li>
+    </ul>)
+  )
+}
+
+export default Accessories_Menu;
