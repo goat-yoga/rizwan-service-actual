936ff591f4fe044e93b0e489179af665b1e52cac
Rizwan Choudhury <choudhuryrizwan97@gmail.com>
2020-06-26 11:05:00 -0700

    adds dropdown menu for women

--- client/src/components/App-Subcomponents/Main_Nav-Subcomponents/Women.jsx
+++ client/src/components/App-Subcomponents/Main_Nav-Subcomponents/Women.jsx
@@ -1,11 +1,29 @@
 import React from 'react';
+import Women_Menu from './Women_Menu.jsx'
 
-const Women = (props) => {
-  return (
-    <li className="main-nav__item">
-      {props.name}
-    </li>
-  )
+export default class Women extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      onHover: false
+    }
+    this.handleHover = this.handleHover.bind(this);
+  }
+
+  handleHover() {
+    this.setState({ onHover: !this.state.onHover })
+  }
+
+  render() {
+    let { onHover } = this.state;
+    return (
+      <li className="main-nav__item"
+        onMouseOver={this.handleHover}
+        onMouseOut={this.handleHover}>
+        <a href="">{this.props.name}</a>
+
+        {onHover ? <Women_Menu /> : null}
+      </li >
+    )
+  }
 }
-
-export default Women;
