1ff2dd33a1948b55190f12e7e94c83b24f572381
Rizwan Choudhury <choudhuryrizwan97@gmail.com>
2020-06-25 18:55:20 -0700

    implement test for next and back arrows in univ banner

--- spec/App-spec/Universal_Banner.test.js
+++ spec/App-spec/Universal_Banner.test.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Universal_Banner from '../../client/src/components/App-Subcomponents/Universal_Banner.jsx';
-import { shallow } from 'enzyme'
+import NextArrow from '../../client/src/components/App-Subcomponents/Universal_Banner-Subcomponents/NextArrow.jsx';
+import BackArrow from '../../client/src/components/App-Subcomponents/Universal_Banner-Subcomponents/BackArrow.jsx';;
+import { shallow, mount } from 'enzyme'
 
 describe('Universal Banner', () => {
   let wrapper;
 
   beforeEach(() => {
-    wrapper = shallow(<Universal_Banner />);
+    wrapper = mount(<Universal_Banner />);
   });
 
 
@@ -18,9 +20,14 @@ describe('Universal Banner', () => {
   })
 
   it('displays next message when next arrow is clicked', () => {
+    wrapper.find("button[name='next']").simulate("click");
     let messages = wrapper.state('messages');
     let index = wrapper.state('messageIndex');
-    let bannerMessage = 'NEW! FRESH BLUE 💙'
+    let bannerMessage = 'Spend $250 and get the Iconic Shopper Tote ($48 value)'
     expect(messages[index]).toBe(bannerMessage);
+
+    wrapper.find("button[name='back']").simulate("click");
+    let newBannerMessage = 'NEW! FRESH BLUE 💙';
+    expect(messages[index]).toBe(newBannerMessage);
   })
 });
