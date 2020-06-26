b9f660fe566f99ae89b6723ffeddfaab0b860538
Rizwan Choudhury <choudhuryrizwan97@gmail.com>
2020-06-25 18:11:27 -0700

    add test dependencies

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,8 @@
 module.exports = {
-  setupFiles: ["<rootDir>/spec/jestSetup.js"]
+  setupFiles: ["<rootDir>/spec/jestSetup.js"],
+  transform: {
+    "^.+\\.jsx?$": "babel-jest"
+  },
+  moduleFileExtensions: ["js", "jsx"],
+  moduleDirectories: ["node_modules", "bower_components"]
 }
