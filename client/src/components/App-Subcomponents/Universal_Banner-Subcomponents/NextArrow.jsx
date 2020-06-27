859cf6e74d99a503027612bf153b91e79f664677
Rizwan Choudhury <choudhuryrizwan97@gmail.com>
2020-06-27 14:17:40 -0700

    add css related dependencies

--- client/src/components/App-Subcomponents/Universal_Banner-Subcomponents/NextArrow.jsx
+++ client/src/components/App-Subcomponents/Universal_Banner-Subcomponents/NextArrow.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
+import NextButton from './nextArrow.svg';
 
 const NextArrow = (props) => {
   return (
     <div className="arrow-link next">
-      <button name="next" onClick={() => props.displayBannerMsg(props.messageIndex, 'next')}> NEXT ARROW </button>
+      <button name="next" onClick={() => props.displayBannerMsg(props.messageIndex, 'next')}>
+        <NextButton />
+      </button>
     </div >
   )
 }
 
 export default NextArrow;
+
--- webpack.config.js
+++ webpack.config.js
@@ -21,6 +21,10 @@ module.exports = {
       {
         test: /\.svg$/,
         use: ['@svgr/webpack'],
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
       }
     ]
   }
