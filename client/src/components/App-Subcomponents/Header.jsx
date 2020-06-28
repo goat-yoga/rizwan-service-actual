46dc176dc5040f072e4e3ef76eca3ecdd9774373
Rizwan Choudhury <choudhuryrizwan97@gmail.com>
2020-06-27 20:44:32 -0700

    add hover styles in header

--- client/src/components/App-Subcomponents/Header.jsx
+++ client/src/components/App-Subcomponents/Header.jsx
@@ -4,18 +4,23 @@ import HeaderElement from './Header-Subcomponents/HeaderElement.jsx';
 export default class Header extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {}
+    this.state = {
+      selectShop: true,
+      selectAloMoves: false,
+      selectAloGives: false,
+      selectStores: false
+    }
   }
 
   render() {
     return (
       <div className="header-container">
         <ul className="header-items-list">
-          <HeaderElement name={'SHOP'} />
-          <HeaderElement name={'ALO MOVES'} />
-          <HeaderElement name={'ALO GIVES'} />
-          <HeaderElement name={'STORES & STUDIOS'} />
-          <li className="header-item">Free 1-2 Day Shipping to California</li>
+          <HeaderElement name={'SHOP'} selected={true} />
+          <HeaderElement name={'ALO MOVES'} selected={false} />
+          <HeaderElement name={'ALO GIVES'} selected={false} />
+          <HeaderElement name={'STORES & STUDIOS'} selected={false} />
+          <li className="header-message">Free 1-2 Day Shipping to California</li>
         </ul>
       </div>
     )
