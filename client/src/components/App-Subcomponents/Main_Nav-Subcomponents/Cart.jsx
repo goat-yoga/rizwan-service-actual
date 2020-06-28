53032a1252a4bba217005f9bab9e83395a8b558d
Rizwan Choudhury <choudhuryrizwan97@gmail.com>
2020-06-28 10:58:48 -0700

    refactor cart component

--- client/src/components/App-Subcomponents/Main_Nav-Subcomponents/Cart.jsx
+++ client/src/components/App-Subcomponents/Main_Nav-Subcomponents/Cart.jsx
@@ -1,12 +1,27 @@
 import React from 'react';
+import DefaultCartLogo from './Cart/Cart_NoHover.svg';
+import HoverCartLogo from './Cart/Cart_Hover.svg';
 
-const Cart = (props) => {
-  return (
-    <li>
-      {props.name}
-      {'image of a cart'}
-    </li>
-  )
+
+export default class Cart extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      cartOnHover: false
+    }
+    this.hoveringOverCart = this.hoveringOverCart.bind(this);
+  }
+
+  hoveringOverCart() { this.setState({ cartOnHover: false }) }
+
+  notHoveringOnCart() { this.setState({ cartOnHover: true }) }
+
+  render() {
+    return (
+      <li>
+        <DefaultCartLogo />
+        <HoverCartLogo />
+      </li>
+    )
+  }
 }
-
-export default Cart;
