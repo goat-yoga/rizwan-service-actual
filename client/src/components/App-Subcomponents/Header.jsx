ac08df342d902213fd7f8cc8b71efb92ae517e46
Rizwan Choudhury <choudhuryrizwan97@gmail.com>
2020-06-27 21:15:53 -0700

    function handling selection not working as expected

--- client/src/components/App-Subcomponents/Header.jsx
+++ client/src/components/App-Subcomponents/Header.jsx
@@ -10,16 +10,51 @@ export default class Header extends React.Component {
       selectAloGives: false,
       selectStores: false
     }
+    this.headerSelected = this.headerSelected.bind(this);
+  }
+
+  headerSelected(headerItem, selection) {
+    if (headerItem === "SHOP") selection = 'selectShop';
+    if (headerItem === "ALO MOVES") selection = 'selectAloMoves';
+    if (headerItem === "ALO GIVES") selection = 'selectAloGives';
+    if (headerItem === "STORES & STUDIOS") selection = 'selectStores';
+
+    this.setState({
+      selectShop: false,
+      selectAloMoves: false,
+      selectAloGives: false,
+      selectStores: false
+    })
+
+    this.setState({ selection: true }, () => { console.log(this.state) })
   }
 
   render() {
+    let { selectShop, selectAloMoves, selectAloGives, selectStores } = this.state;
+
     return (
       <div className="header-container">
         <ul className="header-items-list">
-          <HeaderElement name={'SHOP'} selected={true} />
-          <HeaderElement name={'ALO MOVES'} selected={false} />
-          <HeaderElement name={'ALO GIVES'} selected={false} />
-          <HeaderElement name={'STORES & STUDIOS'} selected={false} />
+          <HeaderElement
+            name={'SHOP'}
+            selected={selectShop}
+            headerSelected={this.headerSelected}
+          />
+          <HeaderElement
+            name={'ALO MOVES'}
+            selected={selectAloMoves}
+            headerSelected={this.headerSelected}
+          />
+          <HeaderElement
+            name={'ALO GIVES'}
+            selected={selectAloGives}
+            headerSelected={this.headerSelected}
+          />
+          <HeaderElement
+            name={'STORES & STUDIOS'}
+            selected={selectStores}
+            headerSelected={this.headerSelected}
+          />
           <li className="header-message">Free 1-2 Day Shipping to California</li>
         </ul>
       </div>
