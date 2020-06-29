a977e0330b016b28ca3258c3b0ae9cb04c8d393a
Rizwan Choudhury <choudhuryrizwan97@gmail.com>
2020-06-29 09:30:31 -0700

    add basic auth menu functionality

--- client/src/components/App-Subcomponents/Main_Nav-Subcomponents/Authentication.jsx
+++ client/src/components/App-Subcomponents/Main_Nav-Subcomponents/Authentication.jsx
@@ -1,12 +1,39 @@
 import React from 'react';
+import NormalAuth from './Authentication/Auth-regular.svg';
+import HoveredAuth from './Authentication/Auth-hovered.svg';
 
-const Authentication = (props) => {
-  return (
-    <li>
-      {props.name}
-      {"image of a person"}
-    </li>
-  )
+export default class Authentication extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      isClicked: false
+    }
+    this.elementIsClicked = this.elementIsClicked.bind(this);
+    this.elementIsNotClicked = this.elementIsNotClicked.bind(this);
+  }
+
+  elementIsClicked() { this.setState({ isClicked: true }) }
+
+  elementIsNotClicked() { this.setState({ isClicked: false }) }
+
+  render() {
+    let { isClicked } = this.state;
+
+    let AuthStatus =
+      (!isClicked) ?
+        <div class="auth-logo">
+          <NormalAuth />
+        </div> :
+
+        <div class="auth-logo selected">
+          <HoveredAuth />
+        </div>;
+
+
+    return (
+      <li >
+        {AuthStatus}
+      </li>
+    )
+  }
 }
-
-export default Authentication;
