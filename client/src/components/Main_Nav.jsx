724e5a90252f33beae936961f3e7fdae3928a33c
Rizwan Choudhury <choudhuryrizwan97@gmail.com>
2020-08-12 17:54:12 -0700

    refactor to functional component

--- client/src/components/Main_Nav.jsx
+++ client/src/components/Main_Nav.jsx
@@ -1,48 +1,41 @@
-import React from 'react';
-import Logo from './Logo.jsx';
-import Search from './Main_Nav-Comp/Search.jsx';
-import Authentication from './Main_Nav-Comp/Authentication.jsx';
-import Cart from './Main_Nav-Comp/Cart.jsx';
+import React from "react";
+import Logo from "./Logo.jsx";
+import Search from "./Main_Nav-Comp/Search.jsx";
+import Authentication from "./Main_Nav-Comp/Authentication.jsx";
+import Cart from "./Main_Nav-Comp/Cart.jsx";
+import MenuElement from "./Main_Nav-Comp/MenuElement.jsx";
 
-import MenuElement from './Main_Nav-Comp/MenuElement.jsx';
+function Main_Nav(props) {
+  return (
+    <div className="main-nav-container">
+      <div className="main-nav-logo">
+        <Logo />
+      </div>
 
-export default class Main_Nav extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {}
-  }
+      <div className="menu">
+        <ul className="menu-list">
+          <li className="main-nav__title">
+            <a className="main-nav__title-name new-in" href="">
+              NEW IN
+            </a>
+          </li>
 
-  render() {
-    return (
-      <div className="main-nav-container">
+          <MenuElement name={"WOMEN"} />
+          <MenuElement name={"MEN"} />
+          <MenuElement name={"ACCESSORIES"} />
+        </ul>
+      </div>
 
-        <div className="main-nav-logo">
-          <Logo />
-        </div>
-
-        <div className="menu">
-          <ul className="menu-list">
-
-            <li className="main-nav__title">
-              <a className="main-nav__title-name new-in" href="">NEW IN</a>
-            </li>
-
-            <MenuElement name={"WOMEN"} />
-            <MenuElement name={"MEN"} />
-            <MenuElement name={"ACCESSORIES"} />
-          </ul>
-        </div>
-
-        <div className="Main-Nav navbar-icons">
-          <ul className="nav-icons-list">
-            <MenuElement name={"WE ARE ALO"} />
-            <Search />
-            <Authentication />
-            <Cart />
-          </ul>
-        </div>
-
-      </div >
-    )
-  }
+      <div className="Main-Nav navbar-icons">
+        <ul className="nav-icons-list">
+          <MenuElement name={"WE ARE ALO"} />
+          <Search />
+          <Authentication />
+          <Cart />
+        </ul>
+      </div>
+    </div>
+  );
 }
+
+export default Main_Nav;
